399067471bf5e0f738337e78969ad183333b05b7
saladt <saladt@seattleu.edu>
2016-01-11 22:56:07 -0500

    made program print to the document

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,24 +1,18 @@
-//create variable that sets the limit for the loop
-var numberLimit = 100;
+var numberLimit = 20;
 
-//create for loop that counts from 1-numberLimit starting at one in increments of 1
+function numFunction(){
+	
 for (number = 1; number <= numberLimit; number++){
-	//check if number is divisible by three
-	if (number % 3 === 0) {
-	//if number is divisible by three check if it is divisible by 5
-	if (number % 5 === 0) {
-	//if this is true, print FizzBuzz instead of the number
-		console.log("FizzBuzz");
-	}else{
-	//if it is not divisible by five but is divisible by three, print Fizz
-		console.log("Fizz");
+	if (number % 3 === 0 && number % 5 === 0){
+		document.write("FizzBuzz" + "<br>");
 	}
-	//if number is divisible by only five print Buzz
-	}else if (number % 5 === 0) {
-		console.log("Buzz");
-	//if number does not meet any of these conditions, print the number
+	else if (number % 3 ===0){
+		document.write("Fizz" + "<br>");
+	}
+	else if (number % 5 === 0){
+		document.write("Buzz" + "<br>");
 	}else{
-		console.log(number);
+		document.write(number + "<br>");
 	}
 }
-
+}
